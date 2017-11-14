d731194a8303c5958df1715f1ca5665bde94b50a
Sam Waller <samuellwaller@gmail.com>
2017-11-14 00:47:36 -0500

    savin hashed pws n stuffs

--- index.js
+++ index.js
@@ -5,12 +5,44 @@ const bodyParser            = require("body-parser")
 const LocalStrategy         = require("passport-local")
 const passportLocalMongoose = require("passport-local-mongoose")
 const User                  = require("./models/user")
-
+///////////////////////////////////
 mongoose.connect("mongodb://localhost/auth_demo")
-
 const app = express()
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}))
+///////////////////////////////////
+app.use(require("express-session")({
+    secret: "Can you keep a secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+/////////////////////////////////////
+
+// Routes ///////////////////////////
+app.get("/register", function(req, res){
+    res.render("register")
+})
+
+app.post("/register", function(req, res){
+    const username = req.body.username
+    const password = req.body.password
+    User.register(new User({username: username}), password, function(err, user){
+        if(err){
+            console.log(err)
+            return res.render("register")
+        }
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret")
+        })
+    })
+})
 
 app.get("/", function(req, res){
     res.render("home")
